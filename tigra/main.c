0bd30938f55a1e159db91cfe7c2bc00f73a7cf0a
Dima <chirkofun@yandex.ru>
2019-05-22 21:13:31 +0300

    Updated. not tested

--- tigra/main.c
+++ tigra/main.c
@@ -4,7 +4,8 @@
 #include <sonar_sensors.h>
 
 #define MIN_DIST 3.8
-#define
+#define WHEEL_BASE 1.24
+#define ARC_LENGTH 2.08
 
 
 int main(void)
@@ -16,8 +17,10 @@ int main(void)
     serial_Init();
 
     uint16_t distanceLeftSonar = 0, placeWidth = 0;
-    float placeLength = 0;
-    bool searchingPlace = true, parking = false;
+    float placeLength = 0, nowDist = 0;
+
+    bool searchingPlace = true, parking1 = false, parking2 = false;
+
     uint16_t firstDistanceLeftSonar = get_sonar_dist(leftSonar, 4);
 
     while (true)
@@ -41,18 +44,63 @@ int main(void)
                 {
                     drive_Speed_Control(0);
                     JENYA_FUNCTION();
+
+                    enc_ticks = 0;
+                    dist = 0;
+                    prev_dist = 0;
+
                     searchingPlace = false;
-                    parking = true;
+                    parking1 = true;
                 }
             }
         }
 
-        if(parking)
+        if(parking1)
         {
             KOLYA_FUNCTION();
+            nowDist = get_distance();
             drive_Direction_Control(false);
-            drive_Speed_Control(1.3);
 
+            if(nowDist > -ARC_LENGTH)
+            {
+                drive_Speed_Control(1.3);
+            }
+            else
+            {
+                JENYA_FUNCTION();
+
+                enc_ticks = 0;
+                dist = 0;
+                prev_dist = 0;
+
+                parking1 = false;
+                parking2 = true;
+            }
+
+        }
+
+        if(parking2)
+        {
+            KOLYA_FUNCTION();
+            nowDist = get_distance();
+            drive_Direction_Control(false);
+
+            if(nowDist > -ARC_LENGTH)
+            {
+                drive_Speed_Control(1.3);
+            }
+            else
+            {
+                JENYA_FUNCTION();
+
+                enc_ticks = 0;
+                dist = 0;
+                prev_dist = 0;
+
+                parking2 = false;
+
+                KOLYA_FUNCTION();
+            }
         }
 
     }
